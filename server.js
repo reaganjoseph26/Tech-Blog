1ad3bddbe6ce93b466a1397751c302bb1a0df7e6
Racer <reaganjoseph26@yahoo.com>
2021-02-06 22:38:46 -0500

    partials fixed

--- server.js
+++ server.js
@@ -4,7 +4,25 @@ const controllers = require('./controllers')
 const sequelize = require('./config/connection');
 
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
 
 
 var app = express();
